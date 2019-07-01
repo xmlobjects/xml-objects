c6c396ded747743622decea7e77f1700fbf640a9
Claus Nagel <cnagel@virtualcitysystems.de>
2019-07-01 21:31:20 +0200

    updated parser result

--- src/main/java/org/xmlobjects/stream/ParseResult.java
+++ src/main/java/org/xmlobjects/stream/ParseResult.java
@@ -1,4 +1,41 @@
 package org.xmlobjects.stream;
 
-public class ParseResult {
+import org.w3c.dom.Node;
+
+public class ParseResult<T> {
+    private final T object;
+    private final Node node;
+
+    private ParseResult(T object, Node node) {
+        this.object = object;
+        this.node = node;
+    }
+
+    public static <T> ParseResult<T> of(T object) {
+        return new ParseResult<>(object, null);
+    }
+
+    public static <T> ParseResult<T> of(Node node) {
+        return new ParseResult<>(null, node);
+    }
+
+    public static <T> ParseResult<T> empty() {
+        return new ParseResult<>(null, null);
+    }
+
+    public boolean isSetObject() {
+        return object != null;
+    }
+
+    public T getObject() {
+        return object;
+    }
+
+    public boolean isSetDOMResult() {
+        return node != null;
+    }
+
+    public Node getDOMResult(){
+        return node;
+    }
 }
