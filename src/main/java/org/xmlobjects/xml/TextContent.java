33a89bd9a705899b18c65039f25dbce21dda6122
Claus Nagel <cnagel@virtualcitysystems.de>
2019-07-03 08:27:10 +0200

    minor change to TextContent

--- src/main/java/org/xmlobjects/xml/TextContent.java
+++ src/main/java/org/xmlobjects/xml/TextContent.java
@@ -1,15 +1,12 @@
 package org.xmlobjects.xml;
 
-import java.util.HashSet;
 import java.util.Objects;
-import java.util.Set;
 
 public class TextContent {
     private String content;
 
     private Object value;
     private String trimmed;
-    private Set<Class<?>> tested;
 
     public TextContent(String content) {
         this.content = Objects.requireNonNull(content, "Content must not be null.");
@@ -26,14 +23,12 @@ public class TextContent {
     }
 
     public boolean isBoolean() {
-        if (value == null && notTested(Boolean.class)) {
-            if ("true".equals(trimmedContent())
-                    || "1".equals(trimmedContent()))
-                value = Boolean.TRUE;
-            else if ("false".equals(trimmedContent())
-                    || "0".equals(trimmedContent()))
-                value = Boolean.FALSE;
-        }
+        if ("true".equals(trimmedContent())
+                || "1".equals(trimmedContent()))
+            value = Boolean.TRUE;
+        else if ("false".equals(trimmedContent())
+                || "0".equals(trimmedContent()))
+            value = Boolean.FALSE;
 
         return value instanceof Boolean;
     }
@@ -43,12 +38,10 @@ public class TextContent {
     }
 
     public boolean isDouble() {
-        if (value == null && notTested(Double.class)) {
-            try {
-                value = Double.parseDouble(trimmedContent());
-            } catch (NumberFormatException e) {
-                //
-            }
+        try {
+            value = Double.parseDouble(trimmedContent());
+        } catch (NumberFormatException e) {
+            //
         }
 
         return value instanceof Double;
@@ -59,12 +52,10 @@ public class TextContent {
     }
 
     public boolean isInteger() {
-        if (value == null && notTested(Integer.class)) {
-            try {
-                value = Integer.parseInt(trimmedContent());
-            } catch (NumberFormatException e) {
-                //
-            }
+        try {
+            value = Integer.parseInt(trimmedContent());
+        } catch (NumberFormatException e) {
+            //
         }
 
         return value instanceof Integer;
@@ -76,13 +67,6 @@ public class TextContent {
 
     private String removeNewLines(String value) {
         return value.replaceAll("\\R", "").replaceAll("\\s+", " ");
-    }
-
-    private boolean notTested(Class<?> type) {
-        if (tested == null)
-            tested = new HashSet<>();
-
-        return tested.add(type);
     }
 
     private String trimmedContent() {
