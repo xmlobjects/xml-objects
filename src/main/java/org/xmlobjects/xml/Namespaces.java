e3d60472efd2f68db978453a9327a3732f919925
Claus Nagel <cnagel@virtualcitysystems.de>
2019-07-07 23:10:29 +0200

    minor change

--- src/main/java/org/xmlobjects/xml/Namespaces.java
+++ src/main/java/org/xmlobjects/xml/Namespaces.java
@@ -3,21 +3,21 @@ package org.xmlobjects.xml;
 import javax.xml.XMLConstants;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
 public class Namespaces {
-    private static final Namespaces EMPTY = new Namespaces();
+    private static final Namespaces EMPTY = new Namespaces(Collections.singleton(XMLConstants.NULL_NS_URI));
     private final Set<String> namespaces;
-
-    private Namespaces() {
-        namespaces = new HashSet<>();
-        namespaces.add(XMLConstants.NULL_NS_URI);
-    }
 
     private Namespaces(Set<String> namespaces) {
         this.namespaces = Objects.requireNonNull(namespaces, "Namespace URIs must not be null.");
+    }
+
+    public static Namespaces newInstance() {
+        return new Namespaces(new HashSet<>());
     }
 
     public static Namespaces of(Collection<String> namespaceURIs) {
@@ -30,6 +30,11 @@ public class Namespaces {
 
     public static Namespaces empty() {
         return EMPTY;
+    }
+
+    public Namespaces add(String namespaceURI) {
+        namespaces.add(namespaceURI);
+        return this;
     }
 
     public boolean contains(String namespaceURI) {
