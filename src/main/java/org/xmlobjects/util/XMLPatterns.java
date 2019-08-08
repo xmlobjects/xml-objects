fff745d7088c1e47f069bae7b0880e629dd7c40b
Claus Nagel <cnagel@virtualcitysystems.de>
2019-08-08 14:43:13 +0200

    added xml:lang pattern

--- src/main/java/org/xmlobjects/util/XMLPatterns.java
+++ src/main/java/org/xmlobjects/util/XMLPatterns.java
@@ -5,4 +5,5 @@ import java.util.regex.Pattern;
 public class XMLPatterns {
     public static final Pattern NAME = Pattern.compile("[_:\\p{L}][-_:.\\p{L}0-9]*");
     public static final Pattern NCNAME = Pattern.compile("[_\\p{L}][-_.\\p{L}0-9]*");
+    public static final Pattern LANGUAGE = Pattern.compile("[a-zA-Z]{1,8}(-[a-zA-Z0-9]{1,8})*");
 }
