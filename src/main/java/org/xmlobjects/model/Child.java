37178c651bbfc2dca8ba2833ba2d1e4ffb469a05
Claus Nagel <cnagel@virtualcitysystems.de>
2020-01-03 16:46:30 +0100

    minor change

--- src/main/java/org/xmlobjects/model/Child.java
+++ src/main/java/org/xmlobjects/model/Child.java
@@ -3,4 +3,14 @@ package org.xmlobjects.model;
 public interface Child {
     Child getParent();
     void setParent(Child parent);
+
+    default <T extends Child> T getParent(Class<T> type) {
+        Child parent = this;
+        while ((parent = parent.getParent()) != null) {
+            if (type.isInstance(parent))
+                return type.cast(parent);
+        }
+
+        return null;
+    }
 }
