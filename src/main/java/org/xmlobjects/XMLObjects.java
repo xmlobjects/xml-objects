53cdc50e461b7c5a4155772e7359aafe24fa9808
Claus Nagel <cnagel@virtualcitysystems.de>
2020-06-02 15:42:20 +0200

    minor change

--- src/main/java/org/xmlobjects/XMLObjects.java
+++ src/main/java/org/xmlobjects/XMLObjects.java
@@ -284,43 +284,41 @@ public class XMLObjects {
     }
 
     private Class<?> findObjectType(ObjectSerializer<?> serializer) throws XMLObjectsException {
-        Class<?> clazz = serializer.getClass();
         Class<?> objectType = null;
 
-        for (Method method : clazz.getMethods()) {
-            if (method.isSynthetic())
-                continue;
+        for (Method method : serializer.getClass().getMethods()) {
+            if (!method.isSynthetic()) {
+                Class<?> candidateType = null;
+                Type[] parameters;
 
-            Class<?> candidateType = null;
-            Type[] parameters;
+                switch (method.getName()) {
+                    case "createElement":
+                        parameters = method.getGenericParameterTypes();
+                        if (parameters.length == 2
+                                && parameters[0] instanceof Class<?>
+                                && parameters[1] == Namespaces.class) {
+                            candidateType = (Class<?>) parameters[0];
+                        }
+                        break;
+                    case "writeChildElements":
+                        parameters = method.getGenericParameterTypes();
+                        if (parameters.length == 3
+                                && parameters[0] instanceof Class<?>
+                                && parameters[1] == Namespaces.class
+                                && parameters[2] == XMLWriter.class) {
+                            candidateType = (Class<?>) parameters[0];
+                        }
+                        break;
+                }
 
-            switch (method.getName()) {
-                case "createElement":
-                    parameters = method.getGenericParameterTypes();
-                    if (parameters.length == 2
-                            && parameters[0] instanceof Class<?>
-                            && parameters[1] == Namespaces.class) {
-                        candidateType = (Class<?>) parameters[0];
-                    }
-                    break;
-                case "writeChildElements":
-                    parameters = method.getGenericParameterTypes();
-                    if (parameters.length == 3
-                            && parameters[0] instanceof Class<?>
-                            && parameters[1] == Namespaces.class
-                            && parameters[2] == XMLWriter.class) {
-                        candidateType = (Class<?>) parameters[0];
-                    }
-                    break;
-            }
+                if (candidateType != null) {
+                    if (objectType != null && candidateType != objectType)
+                        throw new XMLObjectsException("The serializer " + serializer.getClass().getName() +
+                                " uses different object types: " +
+                                objectType.getName() + " and " + candidateType.getName() + ".");
 
-            if (candidateType != null) {
-                if (objectType != null && candidateType != objectType)
-                    throw new XMLObjectsException("The serializer " + serializer.getClass().getName() +
-                            " uses different object types: " +
-                            objectType.getName() + " and " + candidateType.getName() + ".");
-
-                objectType = candidateType;
+                    objectType = candidateType;
+                }
             }
         }
 
