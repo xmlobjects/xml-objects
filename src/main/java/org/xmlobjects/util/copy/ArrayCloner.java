94733a9d522b166d88bb90f2b8be123380e067a1
Claus Nagel <cnagel@virtualcitysystems.de>
2026-03-14 17:24:35 +0100

    added support for copying primitive arrays to copy builder

--- src/main/java/org/xmlobjects/util/copy/ArrayCloner.java
+++ src/main/java/org/xmlobjects/util/copy/ArrayCloner.java
@@ -21,27 +21,31 @@ package org.xmlobjects.util.copy;
 
 import java.lang.reflect.Array;
 
-public class ArrayCloner extends AbstractCloner<Object[]> {
+public class ArrayCloner extends AbstractCloner<Object> {
 
     ArrayCloner(CopyBuilder builder) {
         super(builder);
     }
 
     @Override
-    public Object[] copy(Object[] src, Object[] dest, boolean shallowCopy) {
-        if (shallowCopy || src.getClass().getComponentType().isPrimitive()) {
-            System.arraycopy(src, 0, dest, 0, src.length);
-        } else {
-            for (int i = 0; i < src.length; i++) {
-                dest[i] = deepCopy(src[i]);
+    public Object copy(Object src, Object dest, boolean shallowCopy) {
+        if (src instanceof Object[] srcArray && dest instanceof Object[] destArray) {
+            if (shallowCopy) {
+                System.arraycopy(srcArray, 0, destArray, 0, srcArray.length);
+            } else {
+                for (int i = 0; i < srcArray.length; i++) {
+                    destArray[i] = deepCopy(srcArray[i]);
+                }
             }
+        } else {
+            System.arraycopy(src, 0, dest, 0, Array.getLength(src));
         }
 
         return dest;
     }
 
     @Override
-    public Object[] newInstance(Object[] object, boolean shallowCopy) {
-        return (Object[]) Array.newInstance(object.getClass().getComponentType(), object.length);
+    public Object newInstance(Object object, boolean shallowCopy) {
+        return Array.newInstance(object.getClass().getComponentType(), Array.getLength(object));
     }
 }
